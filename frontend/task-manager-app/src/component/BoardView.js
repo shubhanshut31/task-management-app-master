f4942208d5aac64dccfec086855fff654fa2221d
shubhanshu Tiwari <149867878+shubhanshut31@users.noreply.github.com>
2025-09-24 18:11:01 +0530

    Update API endpoint for fetching tasks

--- frontend/task-manager-app/src/component/BoardView.js
+++ frontend/task-manager-app/src/component/BoardView.js
@@ -10,7 +10,7 @@ export default function BoardViewPage() {
     const [updateTasks, setupdateTasks] = useState(false);
     const [uncompletedTasks, setuncompletedTasks] = useState();
     const getTasks = async () => {
-        const { data } = await axios.get("http://localhost:3002/api/tasks");
+        const { data } = await axios.get("https://task-management-app-master.vercel.app/api/tasks");
         if (data.success === true) {
             const uncompleted =
                 data.data.filter(e => e.completed === false)
