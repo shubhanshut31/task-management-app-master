08d5a578845af5ff21786797416f4bafa94c796b
shubhanshu Tiwari <149867878+shubhanshut31@users.noreply.github.com>
2025-09-24 23:25:15 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,9 @@ mongoose.connect(process.env.DATABASE_URL);
 const app = express();
 const db = mongoose.connection;
 app.use(cors({
-  origin: ["https://task-management-app-master-6yt.vercel.app/"]
+  origin: ["https://task-management-app-master-6yt.vercel.app/",
+          "https://task-management-app-master.vercel.app/",
+          ],
   })
 );
 app.use(express.json());
